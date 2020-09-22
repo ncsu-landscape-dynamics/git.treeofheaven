dd90dfb819abd9cacdaafb49b9b9e64649918514
spacetime-ecology <ben.seliger@gmail.com>
2020-09-22 14:24:42 -0400

    changed paths from local to shared drive

--- Tree_of_Heaven.NE.2.R
+++ Tree_of_Heaven.NE.2.R
@@ -14,44 +14,6 @@ require(mgcv)
 require(ROCR)
 #source('getVars.R')
 
-getVars <- function(res){
-  
-  biodir <- 'H:\\Shared drives\\APHIS  Projects\\shared resources\\data\\worldclim1k\\US\\'
-  biovars <- stack(lapply(X=list.files(biodir), FUN=function(X){raster(paste(biodir, X, sep=''))}))
-  biovars <- crop(biovars, extent(borders)); biovars <- mask(biovars, borders)
-  
-  if(res==30){
-    bio1.30m <- raster('C:\\Users\\bjselige\\Documents\\Tree_of_Heaven\\bio1_NE_30m.tif')
-    canop <- raster('C:\\Users\\bjselige\\Documents\\Tree_of_Heaven\\canop_NE_30m.tif')
-    pop.30m <- raster('C:\\Users\\bjselige\\Documents\\population_density\\popden_NE_30m.tif')
-    rnr.30m <-raster('C:\\Users\\bjselige\\Documents\\RoutesDistance\\rnr_NE_30m.tif')
-    
-    out.stack <- stack(bio1.30m, canop, pop.30m, rnr.30m);
-    names(out.stack) <- c('MAT', 'CAN', 'POP', 'RNR')
-  }
-  
-  if(res==250){
-    bio1.250m <- resample(biovars[[1]], popden, method='ngb')
-    popden <- raster('C:\\Users\\bjselige\\Documents\\population_density\\popden_NE_250m.tif')
-    roads.d <- raster('C:\\Users\\bjselige\\Documents\\RoutesDistance\\roadsD_NE_250m.tif')
-    rails.d <- raster('C:\\Users\\bjselige\\Documents\\RoutesDistance\\railsD_NE_250m.tif')
-  }
-  
-  if(res==1000){
-    bio1.1k <- biovars[[1]]
-    canop.1k <- raster('C:\\Users\\bjselige\\Documents\\Tree_of_Heaven\\canop_NE_1k.tif')
-    pop.1k <-  raster('C:\\Users\\bjselige\\Documents\\population_density\\popden_NE_1k.tif')
-    roads.1k <- raster('C:\\Users\\bjselige\\Documents\\RoutesDistance\\roadsD_NE_1k.tif')
-    rails.1k <- raster('C:\\Users\\bjselige\\Documents\\RoutesDistance\\railsD_NE_1k.tif')
-    rnr.1k <- mosaic(roads.1k, rails.1k, fun='min')
-    
-    out.stack <- stack(bio1.1k, canop.1k, pop.1k, rnr.1k)
-    names(out.stack) <- c('MAT', 'CAN', 'POP', 'RNR')
-  }
-  
-  return(out.stack)
-}
-
 usa <- readOGR('H:\\Shared drives\\APHIS  Projects\\shared resources\\data\\usa_boundaries\\us_lower_48_states.shp')
 usa <- spTransform(usa, CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
 borders <- usa[usa$STATE_NAME%in%c('District of Columbia', 'Delaware', 'New Jersey', 'Maryland', 
@@ -62,6 +24,26 @@ bbox <- as(extent(borders), 'SpatialPolygons'); crs(bbox) <- crs(usa)
 biodir <- 'H:\\Shared drives\\APHIS  Projects\\shared resources\\data\\worldclim1k\\US\\'
 biovars <- stack(lapply(X=list.files(biodir), FUN=function(X){raster(paste(biodir, X, sep=''))}))
 biovars <- crop(biovars, extent(borders)); biovars <- mask(biovars, borders); names(biovars)[1] <- 'MAT'
+
+getVars <- function(res){
+  if(res==30){
+    bio1.30m <- raster('H:\\Shared drives\\Data\\Raster\\Regional\\bio1_NE_30m.tif')
+    canop <- raster('H:\\Shared drives\\Data\\Raster\\Regional\\canop_NE_30m.tif')
+    pop.30m <- raster('H:\\Shared drives\\Data\\Raster\\Regional\\popden_NE_30m.tif')
+    rnr.30m <-raster('H:\\Shared drives\\Data\\Raster\\Regional\\rnr_NE_30m.tif')
+    out.stack <- stack(bio1.30m, canop, pop.30m, rnr.30m)
+  }
+  if(res==1000){
+    canop.1k <- raster('H:\\Shared drives\\Data\\Raster\\Regional\\canop_NE_1k.tif')
+    pop.1k <-  raster('H:\\Shared drives\\Data\\Raster\\Regional\\popden_NE_1k.tif')
+    roads.1k <- raster('H:\\Shared drives\\Data\\Raster\\Regional\\roadsD_NE_1k.tif')
+    rails.1k <- raster('H:\\Shared drives\\Data\\Raster\\Regional\\railsD_NE_1k.tif')
+    rnr.1k <- mosaic(roads.1k, rails.1k, fun='min')
+    out.stack <- stack(biovars[[1]], canop.1k, pop.1k, rnr.1k)
+  }
+  names(out.stack) <- c('MAT', 'CAN', 'POP', 'RNR')
+  return(out.stack)
+}
 
 envi.30 <- getVars(res=30)
 envi.1k <- getVars(res=1000)
