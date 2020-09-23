c99eeec48a95055c9538c0c7ab6fccf1e5b4128d
spacetime-ecology <ben.seliger@gmail.com>
2020-09-23 15:24:10 -0400

    more updates

--- Tree_of_Heaven.NE.2.R
+++ Tree_of_Heaven.NE.2.R
@@ -49,71 +49,48 @@ envi.30 <- getVars(res=30); envi.1k <- getVars(res=1000)
 
 toh.PDA <- read.csv('H:\\Shared drives\\Data\\Table\\Regional\\Ailanthus.PDA.csv')[, c('Longitude', 'Latitude')]
 toh.BIEN <- read.csv('H:\\Shared drives\\Data\\Table\\Global\\Ailanthus.BIEN.csv')[, c('Longitude', 'Latitude')]
-toh.EDD <- read.csv('H:\\Shared drives\\Data\\Table\\USA\\Ailanthus.eddmaps.csv')[-which(is.na(toh.EDD$Longitude)),]
+toh.EDD <- read.csv('H:\\Shared drives\\Data\\Table\\USA\\Ailanthus.eddmaps.csv')
+toh.EDD <- toh.EDD[-which(is.na(toh.EDD$Longitude)),]
 toh.EDD <- toh.EDD[which(toh.EDD$OccStatus=="Positive"), c('Longitude', 'Latitude')]
-toh.all <- SpatialPoints(unique(rbind(toh.PDA, toh.BIEN, toh.EDD)), CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
+toh.FIA <- read.csv('H:\\Shared drives\\Data\\Table\\Regional\\toh.fiaNE.csv')[,c(2,3)]; colnames(toh.FIA) <- c('Longitude', 'Latitude')
+toh.all <- SpatialPoints(unique(rbind(toh.PDA, toh.BIEN, toh.EDD, toh.FIA)), CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
 toh.all <- crop(toh.all, borders)
 
-fiadf <- read.csv('H:\\Shared drives\\Data\\Table\\Regional\\toh.fiaNE.csv', stringsAsFactors = F)
-fia.pts <- SpatialPoints(coords=fiadf[,c(2,3)], CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
-fia.pts <- crop(fia.pts, borders)
+toh.ras <- rasterize(x=toh.all, y=envi.1k$CAN, fun='count', background=NA)
+toh.rtp <- rasterToPoints(toh.ras, spatial=T)
+toh.pts <- SpatialPointsDataFrame(coords=toh.rtp@coords,
+                                  data=data.frame('X'=toh.rtp$layer, 
+                                                  'MAT'=extract(x=envi.1k$MAT, y=toh.rtp),
+                                                  'CAN'=extract(x=envi.1k$CAN, y=toh.rtp),
+                                                  'RNR'=extract(x=envi.1k$RNR, y=toh.rtp),
+                                                  'POP'=extract(x=envi.1k$POP, y=toh.rtp)))
 
-toh.set1 <- toh.all
-toh.set2 <- fia.pts
-toh.set3 <- toh.set1+toh.set2
+k <- 5; set.seed(1991); folds <- kfold(toh.pts, k=k)
 
-k <- 5
-set.seed(1991); folds.set1 <- kfold(toh.set1, k=k)
-set.seed(1991); folds.set2 <- kfold(toh.set2, k=k)
-set.seed(1991); folds.set3 <- kfold(toh.set3, k=k)
-
-test.set1 <- toh.set1[which(folds.set1==k),]
-test.set2 <- toh.set2[which(folds.set2==k),]
-test.set3 <- toh.set3[which(folds.set3==k),]
-
-train.set1 <- toh.set1[which(folds.set1!=k),]
-train.set2 <- toh.set2[which(folds.set2!=k),]
-train.set3 <- toh.set3[which(folds.set3!=k),]
-
-backg.set1 <- spsample(borders, type='regular', n=length(test.set1))
-backg.set2 <- spsample(borders, type='regular', n=length(test.set2))
-backg.set3 <- spsample(borders, type='regular', n=length(test.set3))
-
-ras.set1 <- rasterize(x=toh.set1, y=envi.1k$CAN, fun='count', background=NA)
-rtp.set1 <- rasterToPoints(ras.set1, spatial=T)
-pts.set1 <- SpatialPointsDataFrame(coords=coordinates(rtp.set1),
-                                   data=data.frame('X'=rtp.set1$layer, 
-                                                   'MAT'=extract(x=envi.1k$MAT, y=rtp.set1),
-                                                   'CAN'=extract(x=envi.1k$CAN, y=rtp.set1),
-                                                   'RNR'=extract(x=envi.1k$RNR, y=rtp.set1),
-                                                   'POP'=extract(x=envi.1k$POP, y=rtp.set1)))
+test <- toh.pts[which(folds==k),]
+train <- toh.pts[which(folds!=k),]
+backg <- spsample(borders, type='regular', n=length(test))
 
 ##### Model #####
-m1.gam <- gam(formula=X~MAT+CAN+RNR+POP, data=train.set1@data)
+m1.gam <- gam(formula=X~MAT+CAN+RNR+POP, data=train@data)
 p1.gam <- predict(envi.1k, m1.gam, type='response')
 
-p1.gam2 <- prediction(predictions=extract(p1.gam, pts.set1)[!is.na(extract(p1.gam, pts.set1))],
-                      labels=as.numeric(pts.set1$X>0)[!is.na(extract(p1.gam, pts.set1))])
-e1.gam <- performance(p1.gam2, "auc")
+# p1.gam2 <- prediction(predictions=extract(p1.gam, toh.pts)[!is.na(extract(p1.gam, toh.pts))],
+#                       labels=as.numeric(toh.pts$X>0)[!is.na(extract(p1.gam, toh.pts))])
+# e1.gam <- performance(p1.gam2, "auc")
 
-m2.gam <- gam(formula=X~MAT+CAN+RNR+POP, data=pts.set2@data)
-p2.gam <- predict(envi.1k, m2.gam, type='response')
+m1.rf <- randomForest(formula=X~MAT+CAN+RNR+POP, data=train, na.action = na.omit)
+p1.rf <- predict(envi.1k, m1.rf)
+e1.rf <- evaluate(p=test, a=backg, model=m1.rf, x=envi.1k)
 
-m3.gam <- gam(formula=X~MAT+CAN+RNR+POP, data=pts.set3@data)
-p3.gam <- predict(envi.1k, m3.gam, type='response')
-
-m1.rf <- randomForest(formula=X~MAT+CAN+RNR+POP, data=pts.set1, na.action = na.omit)
-p1.rf <- predict(envi.1k, rf)
-e1.rf <- evaluate(p=test, a=backg, model=rf, x=envi.1k)
-
-mglm <- glm(formula=X~MAT+CAN+RNR+POP, data=all.pts@data)
+mglm <- glm(formula=X~MAT+CAN+RNR+POP, data=train@data)
 pglm <- predict(envi.1k, mglm, type='response')
 eglm <- evaluate(p=test, a=backg, model=mglm, x=envi.1k)
 
-mlm <- glmer(formula=X~CAN+RNR+POP+(1|MAT), data=all.pts@data)
-plm <- predict(envi.1k, mlm, type='response')
+# mlm <- glmer(formula=X~CAN+RNR+POP+(1|MAT), data=train@data)
+# plm <- predict(envi.1k, mlm, type='response')
 
-mgb <- gbm(formula=X~MAT+CAN+RNR+POP, data=all.pts@data)
+mgb <- gbm(formula=X~MAT+CAN+RNR+POP, data=train@data)
 pgb <- predict.gbm(newdata=data.frame(values(envi.1k)), object=mgb, type='response', n.trees=100)
 pgbr <- envi.1k$MAT; values(pgbr) <- pgb
 pgbr <- mask(pgbr, borders)
